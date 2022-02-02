13de26f447140b1dd977bc8041c28e1022912de3
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2022-02-02 14:47:07 +0200

    Initial commit

---
+++ test/cdk-lambda-insights-issue.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaInsightsIssue from '../lib/cdk-lambda-insights-issue-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-insights-issue-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaInsightsIssue.CdkLambdaInsightsIssueStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
